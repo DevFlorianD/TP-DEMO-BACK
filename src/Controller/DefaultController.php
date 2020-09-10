70576f204ecc9bd9e9c0103edbd7f33f3d365023
Florian <florian@archionline.fr>
2020-09-10 16:26:36 +0200

    cool

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     public function index(): JsonResponse
     {
@@ -17,6 +21,32 @@ class DefaultController
                 'completed' => false,
             ],
         ], 200, ['Access-Control-Allow-Origin' => '*']);
+    }
+
+    public function getTodo(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $todo = json_decode($request->request->get('todo'));
+
+
+
+            return new JsonResponse(
+                'OK', 200
+            );
+        } else {
+            $todos = $this->getDoctrine()->getRepository(Todo::class)->findAll();
+
+            return new JsonResponse(
+                $todos, 200
+            );
+        }
+    }
+
+    public function getTodoId(Todo $todo)
+    {
+        return new JsonResponse(
+            $todo, 200
+        );
     }
 }
 
