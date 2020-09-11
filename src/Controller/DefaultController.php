fa72b2dd24e578e6cb8d303bc87392a83de700db
Florian <florian@archionline.fr>
2020-09-11 14:48:32 +0200

    Allow origin

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends AbstractController
 {
     public function index(Request $request): JsonResponse
     {
+        header("Access-Control-Allow-Origin: *");
         if ($request->isMethod('POST')) {
 
             $todo = json_decode($request->getContent(), true);
