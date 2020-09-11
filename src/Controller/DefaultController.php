8956f726392dd916d9c1d63cb168a5772f08f0fb
Florian <florian@archionline.fr>
2020-09-11 14:33:04 +0200

    Test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,15 +12,47 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        return new JsonResponse([
-            [
-                'id' => 1,
-                'title' => 'test 1',
-                'completed' => false,
-            ],
-        ], 200, ['Access-Control-Allow-Origin' => '*']);
+        if ($request->isMethod('POST')) {
+
+            $todo = json_decode($request->getContent(), true);
+
+            $newTodo = new Todo();
+            $newTodo->setIdFront($todo['id']);
+            $newTodo->setTitle($todo['title']);
+            $newTodo->setCompleted($todo['completed']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newTodo);
+            $em->flush();
+
+            return new JsonResponse(
+                'OK',
+                200,
+                ['Access-Control-Allow-Origin' => '*']
+            );
+        } else {
+            $todos = $this->getDoctrine()->getRepository(Todo::class)->findAll();
+
+            $arrayTodos = [];
+
+            foreach ($todos as $todo) {
+                /** @var Todo $todo */
+                $temp = [
+                    'id' => $todo->getId(),
+                    'title' => $todo->getTitle(),
+                    'completed' => $todo->getCompleted(),
+                ];
+
+                array_push($arrayTodos, $temp);
+            }
+
+            return $this->json([
+                $arrayTodos
+            ], 200,
+                ['Access-Control-Allow-Origin' => '*']);
+        }
     }
 
     /**
@@ -30,14 +62,6 @@ class DefaultController extends AbstractController
      */
     public function getTodo(Request $request)
     {
-        return new JsonResponse([
-            [
-                'id' => 1,
-                'title' => 'test 1',
-                'completed' => false,
-            ],
-        ], 200, ['Access-Control-Allow-Origin' => '*']);
-
         if ($request->isMethod('POST')) {
 
             $todo = json_decode($request->getContent(), true);
