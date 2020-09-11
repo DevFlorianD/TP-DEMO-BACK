1fea65ba224a7df8e921fafc60bff7d1e95228a1
Florian <florian@archionline.fr>
2020-09-11 09:03:30 +0200

    api todo

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -26,26 +26,52 @@ class DefaultController extends AbstractController
     public function getTodo(Request $request)
     {
         if ($request->isMethod('POST')) {
-            $todo = json_decode($request->request->get('todo'));
 
+            $todo = json_decode($request->getContent(), true);
 
+            $newTodo = new Todo();
+            $newTodo->setIdFront($todo['id']);
+            $newTodo->setTitle($todo['title']);
+            $newTodo->setCompleted($todo['completed']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newTodo);
+            $em->flush();
 
             return new JsonResponse(
-                'OK', 200
+                'OK',
+                200,
+                ['Access-Control-Allow-Origin' => '*']
             );
         } else {
             $todos = $this->getDoctrine()->getRepository(Todo::class)->findAll();
 
-            return new JsonResponse(
-                $todos, 200
-            );
+            $arrayTodos = [];
+
+            foreach ($todos as $todo) {
+                /** @var Todo $todo */
+                $temp = [
+                    'id' => $todo->getId(),
+                    'title' => $todo->getTitle(),
+                    'completed' => $todo->getCompleted(),
+                ];
+
+                array_push($arrayTodos, $temp);
+            }
+
+            return $this->json([
+                $arrayTodos
+            ], 200,
+            ['Access-Control-Allow-Origin' => '*']);
         }
     }
 
-    public function getTodoId(Todo $todo)
+    public function getTodoId($id)
     {
+        $todo = $this->getDoctrine()->getRepository(Todo::class)->find($id);
+
         return new JsonResponse(
-            $todo, 200
+            $todo, 200, ['Access-Control-Allow-Origin' => '*']
         );
     }
 }
